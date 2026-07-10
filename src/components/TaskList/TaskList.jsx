ee646c8d861963ddde61a1933037c96b019b5c52
Keshav Patel <keshavpatel642@gmail.com>
2026-07-11 04:05:05 +0530

    Improve task list layout

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -6,21 +6,29 @@ import FailedTask from './FailedTask'
 
 const TaskList = ({ data }) => {
     return (
-        <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
+        <div
+            id="tasklist"
+            className="mt-10 flex w-full gap-6 overflow-x-auto pb-5 pt-2"
+        >
             {data.tasks.map((elem, idx) => {
+
                 if (elem.active) {
                     return <AcceptTask key={idx} data={elem} />
                 }
+
                 if (elem.newTask) {
                     return <NewTask key={idx} data={elem} />
                 }
+
                 if (elem.completed) {
                     return <CompleteTask key={idx} data={elem} />
                 }
+
                 if (elem.failed) {
                     return <FailedTask key={idx} data={elem} />
                 }
 
+                return null
             })}
         </div>
     )
